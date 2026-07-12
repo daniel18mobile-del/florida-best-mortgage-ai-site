f54c54b9e09255f641f52e86c2069e83bc5ac8b4
Dan Wolkoff <dan@livingnaples.com>
2026-07-12 18:12:02 -0400

    Add internal links to Spanish pages

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -4,7 +4,8 @@ export const primaryNav = [
   { label: 'Calculators', href: '/calculators/' },
   { label: 'About', href: '/about/' },
   { label: 'Blog', href: '/blog/' },
-  { label: 'Contact', href: '/contact/' }
+  { label: 'Contact', href: '/contact/' },
+  { label: 'Espanol', href: '/es/' }
 ];
 
 export const footerNav = [
@@ -15,6 +16,10 @@ export const footerNav = [
   { label: 'Refinance', href: '/services/refinance-mortgage-florida/' },
   { label: 'Calculators', href: '/calculators/' },
   { label: 'Licensing and Trust', href: '/licensing/' },
+  { label: 'Hipotecas en Espanol', href: '/es/' },
+  { label: 'Servicios en Espanol', href: '/es/services/' },
+  { label: 'Sobre Nosotros en Espanol', href: '/es/about/' },
+  { label: 'Contacto en Espanol', href: '/es/contact/' },
   { label: 'Naples Mortgage Broker', href: '/locations/naples-mortgage-broker/' },
   { label: 'Fort Myers Mortgage Broker', href: '/locations/fort-myers-mortgage-broker/' },
   { label: 'Cape Coral Mortgage Broker', href: '/locations/cape-coral-mortgage-broker/' }
